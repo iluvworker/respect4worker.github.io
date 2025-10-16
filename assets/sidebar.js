5cd8ca803250e7548f42cdfa2472d8be0330152b
iluvworker <eda.ksuke@gmail.com>
2025-10-16 22:14:25 +0900

    Update sidebar.js

--- assets/sidebar.js
+++ assets/sidebar.js
@@ -1,1 +1,16 @@
+// サイドバーを全ページに読み込む
+document.addEventListener("DOMContentLoaded", async () => {
+  const container = document.createElement("div");
+  document.body.prepend(container);
+  
+  const res = await fetch("assets/sidebar.html");
+  const html = await res.text();
+  container.innerHTML = html;
 
+  const sidebar = document.getElementById("sidebar");
+  const toggleBtn = document.getElementById("toggleSidebar");
+
+  toggleBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("hidden");
+  });
+});
